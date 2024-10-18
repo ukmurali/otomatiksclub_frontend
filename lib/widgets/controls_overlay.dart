eef81e178ac6d8967817bee5ae9c260d45ed11f1
Murali <ukmurali@gmail.com>
2024-10-18 18:55:03 +0530

    player UI progress

--- lib/widgets/controls_overlay.dart
+++ lib/widgets/controls_overlay.dart
@@ -1,38 +1,103 @@
 import 'package:flutter/material.dart';
 import 'package:video_player/video_player.dart';
 
-class ControlsOverlay extends StatelessWidget {
+class ControlsOverlay extends StatefulWidget {
   final VideoPlayerController controller;
 
-  const ControlsOverlay({Key? key, required this.controller}) : super(key: key);
+  const ControlsOverlay({super.key, required this.controller});
+
+  @override
+  _ControlsOverlayState createState() => _ControlsOverlayState();
+}
+
+class _ControlsOverlayState extends State<ControlsOverlay> {
+  @override
+  void initState() {
+    super.initState();
+    // Add a listener to update the UI when the video state changes
+    widget.controller.addListener(_updateState);
+  }
+
+  @override
+  void dispose() {
+    // Remove the listener when the widget is disposed
+    widget.controller.removeListener(_updateState);
+    super.dispose();
+  }
+
+  void _updateState() {
+    // Call setState to rebuild the widget when the controller state changes
+    setState(() {});
+  }
+
+  String _formatDuration(Duration duration) {
+    String twoDigits(int n) => n.toString().padLeft(2, '0');
+    String twoDigitMinutes = twoDigits(duration.inMinutes.remainder(60));
+    String twoDigitSeconds = twoDigits(duration.inSeconds.remainder(60));
+    return '${twoDigits(duration.inHours)}:$twoDigitMinutes:$twoDigitSeconds';
+  }
 
   @override
   Widget build(BuildContext context) {
+    Duration currentPosition = widget.controller.value.position;
+    Duration totalDuration = widget.controller.value.duration;
+
     return Stack(
       children: <Widget>[
-        // This GestureDetector will toggle play/pause when the user taps the video.
+        // GestureDetector will toggle play/pause when tapped.
         GestureDetector(
           onTap: () {
-            if (controller.value.isPlaying) {
-              controller.pause();
+            if (widget.controller.value.isPlaying) {
+              widget.controller.pause();
             } else {
-              controller.play();
+              widget.controller.play();
             }
           },
-          // Ensure the icons are centered and always visible when paused.
           child: Center(
             child: AnimatedOpacity(
-              opacity: controller.value.isPlaying ? 0.0 : 1.0,
+              opacity: widget.controller.value.isPlaying ? 0.0 : 1.0,
               duration: const Duration(milliseconds: 300),
-              // Icon size is large and in white for contrast.
               child: Icon(
-                controller.value.isPlaying
-                    ? Icons.pause_circle_filled
-                    : Icons.play_circle_filled,
-                color: Colors.white, // Make sure the icon is clearly visible
+                // Show pause icon when playing and play icon otherwise
+                widget.controller.value.isPlaying
+                    ? Icons.pause // Pause icon
+                    : Icons.play_arrow, // Play icon
+                color: Colors.white, // Set the icon color to black
                 size: 80.0, // Large size to make it noticeable
               ),
             ),
+          ),
+        ),
+        // Display the current time and total duration
+        Positioned(
+          bottom: 30,
+          left: 16,
+          right: 16,
+          child: Column(
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    _formatDuration(currentPosition),
+                    style: const TextStyle(color: Colors.white),
+                  ),
+                  Text(
+                    _formatDuration(totalDuration),
+                    style: const TextStyle(color: Colors.white),
+                  ),
+                ],
+              ),
+              // Optionally, you can add a Slider here to allow seeking
+              Slider(
+                value: currentPosition.inSeconds.toDouble(),
+                min: 0,
+                max: totalDuration.inSeconds.toDouble(),
+                onChanged: (value) {
+                  widget.controller.seekTo(Duration(seconds: value.toInt()));
+                },
+              ),
+            ],
           ),
         ),
       ],
--- lib/widgets/create_post_dialog_mobile.dart
+++ lib/widgets/create_post_dialog_mobile.dart
@@ -1,6 +1,6 @@
 import 'dart:io';
+import 'dart:typed_data';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:permission_handler/permission_handler.dart';
 import 'package:stem_club/colors/app_colors.dart';
@@ -46,28 +46,23 @@ class _CreatePostDialogMobileState extends State<CreatePostDialogMobile> {
 
     try {
       // Separate logic for picking images and videos
-      if (source == ImageSource.camera || source == ImageSource.gallery) {
-        var pickedFile =
-            await picker.pickImage(source: source, imageQuality: 100);
-        if (pickedFile != null) {
-          final bytes = await pickedFile.readAsBytes();
-          setState(() {
-            imageBytes = bytes;
-            _videoController?.dispose();
-            _videoController = null;
-          });
-        } else {
-          // Pick video if no image was picked
-          var pickedVideo = await picker.pickVideo(source: source);
-          if (pickedVideo != null) {
-            _videoController =
-                VideoPlayerController.file(File(pickedVideo.path))
-                  ..initialize().then((_) {
-                    setState(() {
-                      _videoController!.play(); // Start playing the video
-                    });
-                  });
-          }
+      var pickedFile =
+          await picker.pickImage(source: source, imageQuality: 100);
+      if (pickedFile != null) {
+        final bytes = await pickedFile.readAsBytes();
+        setState(() {
+          imageBytes = bytes;
+          _videoController?.dispose();
+          _videoController = null;
+        });
+      } else {
+        // Pick video if no image was picked
+        var pickedVideo = await picker.pickVideo(source: source);
+        if (pickedVideo != null) {
+          _videoController = VideoPlayerController.file(File(pickedVideo.path))
+            ..initialize().then((_) {
+              setState(() {});
+            });
         }
       }
     } catch (e) {
@@ -92,30 +87,6 @@ class _CreatePostDialogMobileState extends State<CreatePostDialogMobile> {
     );
   }
 
-  void _showSettingsDialog(String permissionName) {
-    showDialog(
-      context: context,
-      builder: (context) => AlertDialog(
-        title: Text('$permissionName Permission Permanently Denied'),
-        content: Text(
-            'This app requires $permissionName permission to continue. Please go to the app settings to enable it.'),
-        actions: [
-          TextButton(
-            onPressed: () {
-              openAppSettings();
-              Navigator.of(context).pop();
-            },
-            child: const Text('Open Settings'),
-          ),
-          TextButton(
-            onPressed: () => Navigator.of(context).pop(),
-            child: const Text('Cancel'),
-          ),
-        ],
-      ),
-    );
-  }
-
   void _resetMedia() {
     setState(() {
       imageBytes = null;
@@ -127,7 +98,6 @@ class _CreatePostDialogMobileState extends State<CreatePostDialogMobile> {
   @override
   void dispose() {
     _videoController?.dispose();
-    _resetMedia();
     titleController.dispose();
     descriptionController.dispose();
     super.dispose();
@@ -145,16 +115,14 @@ class _CreatePostDialogMobileState extends State<CreatePostDialogMobile> {
           icon: const Icon(Icons.close),
           onPressed: () {
             _resetMedia();
-            // Close the dialog
             Navigator.of(context).pop();
           },
         ),
         actions: [
           Padding(
-            padding: const EdgeInsets.only(right: 16.0), // Add right padding
+            padding: const EdgeInsets.only(right: 16.0),
             child: ElevatedButton(
               onPressed: () {
-                // Handle post creation logic here
                 _resetMedia();
                 Navigator.of(context).pop();
               },
@@ -175,7 +143,6 @@ class _CreatePostDialogMobileState extends State<CreatePostDialogMobile> {
           children: [
             Expanded(
               child: SingleChildScrollView(
-                // Wrap in SingleChildScrollView
                 child: Padding(
                   padding: const EdgeInsets.all(16.0),
                   child: Column(
@@ -193,14 +160,13 @@ class _CreatePostDialogMobileState extends State<CreatePostDialogMobile> {
                         labelText: 'Description',
                         keyboardType: TextInputType.multiline,
                         readOnly: false,
-                        maxLines: null, // Allow multiple lines
+                        maxLines: null,
                         showCounter: false,
                       ),
                       const SizedBox(height: 16),
                       if (imageBytes != null)
                         ClipRRect(
-                          borderRadius: BorderRadius.circular(
-                              5.0), // Set corner radius here
+                          borderRadius: BorderRadius.circular(5.0),
                           child: Image.memory(
                             imageBytes!,
                             height: 500,
@@ -209,22 +175,33 @@ class _CreatePostDialogMobileState extends State<CreatePostDialogMobile> {
                         ),
                       if (_videoController != null &&
                           _videoController!.value.isInitialized)
-                        Column(
-                          children: [
-                            AspectRatio(
-                              aspectRatio: _videoController!.value.aspectRatio,
-                              child: Stack(
-                                alignment: Alignment.bottomCenter,
-                                children: [
-                                  VideoPlayer(_videoController!),
-                                  ControlsOverlay(
-                                      controller: _videoController!),
-                                  VideoProgressIndicator(_videoController!,
-                                      allowScrubbing: true),
-                                ],
+                        ClipRRect(
+                          borderRadius: BorderRadius.circular(5.0),
+                          child: Column(
+                            children: [
+                              SizedBox(
+                                width: screenWidth,
+                                height:
+                                    500, // Desired height for the video player
+                                child: AspectRatio(
+                                  aspectRatio:
+                                      _videoController!.value.aspectRatio,
+                                  child: Stack(
+                                    alignment: Alignment.bottomCenter,
+                                    children: [
+                                      VideoPlayer(_videoController!),
+                                      ControlsOverlay(
+                                          controller: _videoController!),
+                                      VideoProgressIndicator(
+                                        _videoController!,
+                                        allowScrubbing: true,
+                                      ),
+                                    ],
+                                  ),
+                                ),
                               ),
-                            ),
-                          ],
+                            ],
+                          ),
                         ),
                     ],
                   ),
@@ -240,7 +217,6 @@ class _CreatePostDialogMobileState extends State<CreatePostDialogMobile> {
                   IconButton(
                     icon: const Icon(Icons.camera_alt, color: Colors.black),
                     onPressed: () {
-                      // Handle send action
                       _pickMedia(ImageSource.camera);
                     },
                   ),
